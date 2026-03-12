f4e59a984ac70263a07418042c16fc40639d7597
Zakaria171 <zakaria.rabih93@gmail.com>
2026-03-12 16:06:05 +0000

    refactor: model product.template

---
+++ addons_custom/catalog_extension/models/product_template.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    badge_label = fields.Char(string = "Badge")
+    warranty_months = fields.Integer(string = "Warranty (Months)")
+    product_origin = fields.Selection(string = "Origin")
+    is_promo_active = fields.Boolean(string = "Promo Active")
