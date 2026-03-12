24653b06fb3ac753a095826282a7c768648bacc8
Zakaria171 <zakaria.rabih93@gmail.com>
2026-03-12 16:09:49 +0000

    refactor: mannifest.py and product_template_view.xml

--- addons_custom/catalog_extension/__manifest__.py
+++ addons_custom/catalog_extension/__manifest__.py
@@ -1,4 +1,11 @@
 {
     "name" : "catalog_extension",
-    "version" : "17.0.1.0"
+    "version" : "17.0.1.0",
+    "depends" : ["product"],
+    "license": "LGPL-3",
+    "data" : [
+        "views/product_template_views.xml"
+        ],
+    "installable" : True,
+    "application" : False
 }
