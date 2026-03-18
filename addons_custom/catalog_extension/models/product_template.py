b81ef6975beb718158201a30fb92c9124143ad53
Zakaria171 <zakaria.rabih93@gmail.com>
2026-03-18 04:46:04 +0000

    feat : add product fields from view

--- addons_custom/catalog_extension/models/product_template.py
+++ addons_custom/catalog_extension/models/product_template.py
@@ -5,5 +5,11 @@ class ProductTemplate(models.Model):
 
     badge_label = fields.Char(string = "Badge")
     warranty_months = fields.Integer(string = "Warranty (Months)")
-    product_origin = fields.Selection(string = "Origin")
+    product_origin = fields.Selection(
+        selection=[
+        ("morocco", "Morocco"),
+        ("france", "France"),
+        ("china", "China"),
+        ],
+        string = "Origin")
     is_promo_active = fields.Boolean(string = "Promo Active")
